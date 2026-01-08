6877486aa78b90f0cc5aaf06d3f24789d0ec272d
bbrunell <brunelle221@gmail.com>
2026-01-08 18:00:37 +0100

    save fixture in the db
    rails db:reset
    rails db:migrate
    rails db:fixtures:load

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_08_163734) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_08_163921) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -37,6 +37,19 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_08_163734) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "customer_id", null: false
+    t.date "from_date"
+    t.string "status"
+    t.text "status_reason"
+    t.date "to_date"
+    t.datetime "updated_at", null: false
+    t.integer "vehicle_id", null: false
+    t.index ["customer_id"], name: "index_bookings_on_customer_id"
+    t.index ["vehicle_id"], name: "index_bookings_on_vehicle_id"
   end
 
   create_table "cars", force: :cascade do |t|
@@ -94,6 +107,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_08_163734) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "users", column: "customer_id"
+  add_foreign_key "bookings", "vehicles"
   add_foreign_key "cars", "vehicles"
   add_foreign_key "motorbikes", "vehicles"
   add_foreign_key "sessions", "users"
